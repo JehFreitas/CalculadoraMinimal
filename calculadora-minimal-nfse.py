4736964655b445e176fdc97dfa4baf894a9d4a56
JehFreitas <jessyca.freitas@minimaldesign.com.br>
2025-05-12 08:07:42 -0300

    Update calculadora-minimal-nfse.py

--- calculadora-minimal-nfse.py
+++ calculadora-minimal-nfse.py
@@ -270,6 +270,7 @@ if submit:
     ("Distância:", str(km_ida_volta)),
     ("Horario:", horario),
     ("Inscrição Estadual:", tem_ie),
+    ("Valor dos Produtos:", str(valor_produtos)),
     ]:
         linha -= 15
         c.setFont("Helvetica-Bold", 10)
